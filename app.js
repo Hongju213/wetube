05cd85c6bfbd44830214e3e7c1fbbb04a3084765
HongjuKim <hjkim@kimids.co.kr>
2021-01-25 08:46:45 +0900

    0125

--- app.js
+++ app.js
@@ -11,15 +11,12 @@ import globalRouter from "./routers/globalRouter";
 
 const app = express();
 
+app.use(helmet());
 app.set("view engine", "pug");
+app.use("/uploads", express.static("uploads"));
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
-app.use(helmet());
-app.use(function(req, res, next) {
-    res.setHeader("Content-Security-Policy", "script-src 'self' https://archive.org");
-    return next();
-    });
 app.use(morgan("dev"))
 app.use(localsMiddleware);
 
