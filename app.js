51c77644ad99f8facc988eb3276fd15d06e8d5d5
HongjuKim <hjkim@kimids.co.kr>
2021-01-30 15:18:15 +0900

    0130

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import passport from "passport";
 import { localsMiddleware } from "./middlewares";
 import routes from "./routes";
 import userRouter from "./routers/userRouter";
@@ -22,8 +23,10 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(morgan("dev"))
-app.use(localsMiddleware);
+app.use(passport.initialize());
+app.use(passport.session());
 
+app.use(localsMiddleware);
 
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
